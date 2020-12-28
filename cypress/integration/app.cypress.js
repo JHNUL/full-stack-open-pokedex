f24a06494e13b509de93eb52dec5b1d70fa79bdb
Juhani Riisiö <juhani.riisio@gmail.com>
2020-12-28 11:36:08 +0200

    add second e2e test

--- cypress/integration/app.cypress.js
+++ cypress/integration/app.cypress.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('can navigate to pokemon page', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('.list-item-name').eq(1).click()
+    cy.contains('chlorophyll')
+  })
 })
