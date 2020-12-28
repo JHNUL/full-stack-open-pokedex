026109a4597d7ef9fdb62b2e331824a5b7c15eba
Juhani Riisiö <juhani.riisio@gmail.com>
2020-12-28 13:05:16 +0200

    add health check

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express = require('express')
 const app = express()
 
+const express = require('express')
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (_, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_, res) => {
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line
-  console.log('server started on port 5000')
+  console.log("server started on port 5000");
 })
